4bc0627902383772a40a9b9fd53e9670aee29c66
Sandra-Su <sandrasuarez1705@gmail.com>
2019-12-26 13:56:19 -0600

    Detalles del producto por su id

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product;
+  constructor(
+    private route: ActivatedRoute, //es específico para cada componente enrutado cargado por el enrutador angular. Contiene información sobre la ruta, sus parámetros y datos adicionales asociados con la ruta.
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productId')];
+  });
   }
 
 }
